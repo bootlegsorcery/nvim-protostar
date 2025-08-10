310daf6e28a18b3fa9ec6dbcbc1503e41f36ae7d
bootlegsorcery <21298833+bootlegsorcery@users.noreply.github.com>
2025-08-11 00:21:06 +1000

    Feat: Add auto indent and comment shortcuts

--- init.lua
+++ init.lua
@@ -17,3 +17,24 @@ vim.opt.smartindent = true
 
 -- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
+
+
+
+
+-- Set-up Super + / for commenting
+require('Comment').setup()
+
+-- Normal mode: toggle comment on the current line
+vim.keymap.set('n', '<D-/>', function()
+  require('Comment.api').toggle.linewise.current()
+end, { noremap = true, silent = true })
+
+-- Visual mode: toggle comment on the selected lines
+vim.keymap.set('v', '<D-/>', function()
+  -- Get the visual mode type
+  local esc = vim.api.nvim_replace_termcodes('<Esc>', true, false, true)
+  local mode = vim.fn.visualmode()
+  -- Exit visual mode first to avoid issues
+  vim.api.nvim_feedkeys(esc, 'n', false)
+  require('Comment.api').toggle.linewise(mode)
+end, { noremap = true, silent = true })
